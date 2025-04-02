eac7a30dff95517edfb0de70de6cc7337015b29b
Sudesh Arosha Seneviratne (SAS) <sudeshseneviratne@gmail.com>
2025-04-03 02:37:01 +0530

    1

---
+++ lkgeektrading/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <div className="flex flex-col items-center justify-center h-screen">
+        <h1 className="text-4xl font-bold">LK Geek Trading</h1>
+      </div>
+    </>
+  );
+}
